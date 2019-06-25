47123be03cfd2758d98a6fdb2b74df299f4c5934
Raj Lone <Rajlone@outlook.com>
2019-06-25 01:01:23 -0400

    WIP

---
+++ Src/SmartRecord/SmartRecord/SmartRecord/Services/Call/CallRecorder.cs
@@ -0,0 +1,45 @@
+﻿using Android.Media;
+using Android.Telephony;
+using System;
+using System.Threading.Tasks;
+
+namespace SmartRecord.Services.Call
+{
+    public class CallRecorder : ICallRecorder
+    {
+        public MediaRecorder recorder = new MediaRecorder();
+        TelephonyManager t;
+
+        public Task<bool> PauseRecording()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ResumeRecording()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> StartRecording()
+        {
+            
+            recorder.Reset();
+            recorder.SetAudioSource(AudioSource.VoiceCall);
+            recorder.SetOutputFormat(OutputFormat.ThreeGpp);
+            recorder.SetAudioEncoder(AudioEncoder.Aac);
+            recorder.SetOutputFile("/sdcard/Download/123.3gpp");
+            recorder.Prepare();
+            recorder.Start();
+           
+            return true;
+            
+        }
+
+        public async Task<bool> StopRecording()
+        {
+            recorder.Stop();
+            recorder.Release();
+            return true;
+        }
+    }
+}
---
+++ Src/SmartRecord/SmartRecord/SmartRecord/Services/Call/ICallRecorder.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartRecord.Services.Call
+{
+    public interface ICallRecorder
+    {
+        Task<bool> StartRecording();
+        Task<bool> StopRecording();
+        Task<bool> PauseRecording();
+        Task<bool> ResumeRecording();
+    }
+}
